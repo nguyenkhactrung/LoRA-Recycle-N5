acebb4af1a9cc7e84b8172c7acd562804c583c37
Thanh-to1409 <146173075+Thanh-to1409@users.noreply.github.com>
2025-10-21 12:48:47 +0700

    Update code
    Refactor CombineWholeFileRecordReader to improve clarity and functionality.

--- datasets/hadoop_jobs/CombineWholeFileInputFormat.java
+++ datasets/hadoop_jobs/CombineWholeFileInputFormat.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
@@ -7,93 +6,86 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.IOUtils;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.InputSplit;
-import org.apache.hadoop.mapreduce.JobContext;
-import org.apache.hadoop.mapreduce.RecordReader;
-import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.CombineFileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.CombineFileRecordReader;
 import org.apache.hadoop.mapreduce.lib.input.CombineFileSplit;
 
 /**
- * Gom nhiều file nhỏ thành 1 split để giảm số mapper.
+ * Đọc nhiều file nhỏ (ảnh) thành cặp key/value:
+ * key = tên file (Text)
+ * value = nội dung ảnh (BytesWritable)
  */
 public class CombineWholeFileInputFormat extends CombineFileInputFormat<Text, BytesWritable> {
 
     @Override
     protected boolean isSplitable(JobContext context, Path file) {
-        return false; // không chia nhỏ file
+        return false;
     }
 
     @Override
-    public RecordReader<Text, BytesWritable> createRecordReader(
-            InputSplit split, TaskAttemptContext context) throws IOException {
-        return new CombineWholeFileRecordReader((CombineFileSplit) split, context);
+    public RecordReader<Text, BytesWritable> createRecordReader(InputSplit split, TaskAttemptContext context)
+            throws IOException {
+        return new CombineFileRecordReader<>((CombineFileSplit) split, context, CombineWholeFileRecordReader.class);
     }
 
     public static class CombineWholeFileRecordReader extends RecordReader<Text, BytesWritable> {
         private CombineFileSplit split;
         private TaskAttemptContext context;
         private int index;
-        private Text key = new Text();
-        private BytesWritable value = new BytesWritable();
+        private Text currentKey;
+        private BytesWritable currentValue;
+        private boolean processed = false;
 
-        public CombineWholeFileRecordReader(CombineFileSplit split, TaskAttemptContext context) {
+        public CombineWholeFileRecordReader(CombineFileSplit split, TaskAttemptContext context, Integer index) {
             this.split = split;
             this.context = context;
-            this.index = 0;
+            this.index = index;
         }
 
         @Override
         public void initialize(InputSplit split, TaskAttemptContext context) throws IOException {
-            this.split = (CombineFileSplit) split;
-            this.context = context;
-            this.index = 0;
+            // Không cần làm gì thêm
         }
 
         @Override
         public boolean nextKeyValue() throws IOException {
-            if (index < split.getNumPaths()) {
-                Path file = split.getPath(index);
-                FileSystem fs = file.getFileSystem(context.getConfiguration());
-                FileStatus status = fs.getFileStatus(file);
+            if (processed)
+                return false;
 
-                long fileLength = status.getLen();
-                if (fileLength > Integer.MAX_VALUE) {
-                    throw new IOException("File too large: " + file.toString());
-                }
+            Path file = split.getPath(index);
+            FileSystem fs = file.getFileSystem(context.getConfiguration());
+            FileStatus status = fs.getFileStatus(file);
+            byte[] contents = new byte[(int) status.getLen()];
 
-                byte[] contents = new byte[(int) fileLength];
-                try (FSDataInputStream in = fs.open(file)) {
-                    IOUtils.readFully(in, contents, 0, contents.length);
-                    value.set(contents, 0, contents.length);
-                }
-
-                // key = chỉ lấy tên file (000001.jpg) thay vì full path
-                key.set(file.getName());
-                index++;
-                return true;
+            try (FSDataInputStream in = fs.open(file)) {
+                IOUtils.readFully(in, contents, 0, contents.length);
             }
-            return false;
+
+            currentKey = new Text(file.getName());
+            currentValue = new BytesWritable(contents);
+            processed = true;
+            return true;
         }
 
         @Override
         public Text getCurrentKey() {
-            return key;
+            return currentKey;
         }
 
         @Override
         public BytesWritable getCurrentValue() {
-            return value;
+            return currentValue;
         }
 
         @Override
         public float getProgress() {
-            return (float) index / split.getNumPaths();
+            return processed ? 1.0f : 0.0f;
         }
 
         @Override
         public void close() throws IOException {
-            // nothing to close
+            // nothing
         }
     }
 }
